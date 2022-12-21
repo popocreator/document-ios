661f975b00bfa9bbb390c6635607fd575ce641a3
popomon <popomonclub@gmail.com>
2022-12-21 23:36:16 +0900

    docs: Added `struct method` guide

---
+++ example/ex_20_struct_method.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+struct Friend {
+    var age : Int
+    var name : String
+    
+    func sayHello() -> String {
+        print("sayHello()")
+        return "저는 \(age)살 \(name)입니다."
+    }
+}
+
+var myFriend = Friend(age: 10, name: "정대리")
+myFriend.sayHello()
